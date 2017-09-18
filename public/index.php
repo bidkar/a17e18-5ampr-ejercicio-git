a9bddcc0d7b1eded7711d058c476dd9b29669c47
Bidkar Aragon Cardenas <bidkar@cetis108.edu.mx>
2017-09-18 09:33:35 -0600

    Metodo FindById()
    Se creo el metodo FindById() para realizar busquedas en la tabla user con el parametro del campo id

--- public/index.php
+++ public/index.php
@@ -3,6 +3,37 @@ require_once '../src/app/db/mysql.php';
 require_once '../src/models/user.php';
 use Models\User;
 
-$usuario = new User();
-$r = $usuario->FindById(1);
-var_dump($r);
+$id = 1;
+$usuario = User::FindById($id);
+// var_dump($usuario);
+
+if (!$usuario) {
+    // imprimir que no lo encontro
+    echo 'Usuario con id='.$id.' no encontrado!';
+} else {
+    // imprimir informacion del usuario
+    // table>(thead>tr>th*5)+(tbody>tr>td*5)
+?>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>FIRSTNAME</th>
+                <th>LASTNAME</th>
+                <th>EMAIL</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><?php echo $usuario->id; ?></td>
+                <td><?php echo $usuario->name; ?></td>
+                <td><?php echo $usuario->firstname; ?></td>
+                <td><?php echo $usuario->lastname; ?></td>
+                <td><?php echo $usuario->email; ?></td>
+            </tr>
+        </tbody>
+    </table>
+<?php
+}
+?>
--- src/models/user.php
+++ src/models/user.php
@@ -3,34 +3,28 @@ namespace Models;
 use App\DB\MySQL;
 
 class User {
-    public  $id;
-    private $name;
-    private $firstname;
-    private $lastname;
-    private $email;
-    private $password;
+    private $data =
+        ['id'=>'',
+        'name'=>'',
+        'firstname'=>'',
+        'lastname'=>'',
+        'email'=>''];
 
-    function FindById($id) {
-        // crear una conexion a MySQL
-        $cnn = new MySQL(); // $cnn es un objeto de tipo mysqli con conexion a base de datos bibliotecam
+    public function __get($var) {
+        return $this->data[$var];
+    }
 
-        // definir la consulta
+    public function __set($var, $value) {
+        if (array_key_exists($var, $this->data))
+            $this->data[$var] = $value;
+    }
+
+    public static function FindById($id) {
+        $cnn = new MySQL();
         $sql = sprintf("select * from users where id=%d",$id);
-
-        // ejecutar la consulta
         $rst = $cnn->query($sql);
-        /*
-         * el metodo query() de la clase mysqli recibe como parametro la
-         * consulta a ejecutar, con una conexion abierta, los posibles
-         * resultados son: bool(false) para cuando falla la conexion,
-         * ejecucion, sintaxis, etc de la consulta, en caso de no fallar
-         * si la consulta es SELECT, SHOW, EXPLAIN el resultado seria
-         * un objeto de tipo mysqli_result, y en caso de ser INSERT,
-         * UPDATE, DELETE retorna bool(true)
-         */
-
-        // evaluar los resultados
-        if (!$rst) { // pregunta si fallo la conexion, ejecucion
+        
+        if (!$rst) {
             die('Error en la consulta');
         } elseif ($rst->num_rows == 1) {
             $u = $rst->fetch_assoc();
