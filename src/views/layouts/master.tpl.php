b2611b15f4aa5c5ca133abc69ad522a4839bddeb
Bidkar Aragon Cardenas <bidkar@cetis108.edu.mx>
2017-09-29 09:26:54 -0600

    Bootstrap 3
    Se agregó bootstrap css framework

---
+++ src/views/layouts/master.tpl.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Inicio de sesión</title>
+    <link rel="stylesheet" href="../../public/css/bootstrap.css">
+    <link rel="stylesheet" href="../../public/css/bootstrap-theme.css">
+</head>
+<body>
+    <!-- Bloque para definición de vistas -->
+</body>
+</html>
