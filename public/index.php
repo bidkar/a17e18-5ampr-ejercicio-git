8fb198b4ce82740668be78ae5e0970d8612d8790
bidkar <bidkar@gmail.com>
2017-10-25 07:51:32 -0600

    Cambios viejitos
    Instrucciones de configuración de virtual hosts de apache.

--- public/index.php
+++ public/index.php
@@ -1,19 +1,17 @@
 <?php
 require_once '../config.inc.php';
 require_once '../src/app/autoload.php';
-use Src\App\Router;
+?>
 
-Router::get('/', 'home@index');
-
-// if (isset($_GET['url'])) {
-//     switch ($_GET['url']) {
-//         case 'home': include '../src/views/home.php';
-//                      break;
-//         case 'user': echo 'Estas pidiendo user';
-//                      break;
-//         case 'login': include '../src/views/login.php';
-//                       break;
-//     }
-// } else {
-//     include '../src/views/home.php';
-// }
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Biblioteca CETis108</title>
+</head>
+<body>
+  <h1>Biblioteca CETis108</h1>
+</body>
+</html>
--- src/app/router.php
+++ src/app/router.php
@@ -1,22 +1,22 @@
 <?php
 namespace Src\App;
 
-use Src\Controllers\HomeController;
-
 class Router {
+    private $controller = '';
+    private $action = '';
+    private $param = '';
+
     // /controller/action/params
-    public static function get($route, $controller, $param = null) {
-        if (!isset($_GET['url'])) {
-            $ctl = preg_split('/[@]/', $controller);
-            $call = $ctl[0].'Controller::'.$ctl[1].'()';
-            call_user_func($call);
-        } else {
-            $url = preg_split('/[\/]/', $_GET['url']);
-            var_dump($url);
-        }
+    public static function get($url) {
+        $x = preg_split('/[\/]/', $url);
+        $controller = $x[0];
+        $action = $x[1];
+        $param = $x[2];
+        var_dump($x);
+        return 0;
     }
 
-    public static function post($route, $controller) {
-
+    public static function post($url) {
+        return 0;
     }
 }
